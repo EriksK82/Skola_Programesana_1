f3b35deccd3ef3cd6f3a6fcff265e40809a76ab8
EriksK82 <eriks.karklins82@gmail.com>
2023-01-24 20:17:05 +0200

    1_3

---
+++ Eksamens/1_3/1_3/main.cpp
@@ -0,0 +1,28 @@
+//
+//  main.cpp
+//  1_3
+//
+//  Created by Eriks Karklins on 24/01/2023.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    double x,y,z,v;
+    
+    cout<<" ievadiet skaitli"<<endl;
+    cin>>x;
+    cout<<" ievadiet skaitli"<<endl;
+    cin>>y;
+    cout<<" ievadiet skaitli"<<endl;
+    cin>>z;
+    
+    v=(x+y+z)/3;
+    
+    cout<<" Vidējais skait;is "<< v<< endl;
+    
+    
+    
+    return 0;
+}
