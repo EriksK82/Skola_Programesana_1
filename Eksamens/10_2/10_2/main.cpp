3530d4119b1e215bc9a7b939bf0b063dd236213f
EriksK82 <eriks.karklins82@gmail.com>
2023-01-28 20:09:03 +0200

    10_2

---
+++ Eksamens/10_2/10_2/main.cpp
@@ -0,0 +1,45 @@
+//
+//  main.cpp
+//  10_2
+//
+//  Created by Eriks Karklins on 28/01/2023.
+//Ievadiet divus skaitļus un simbolu. Atkarībā no ievadītā simbola: + - * / (сhar) jāizpilda kādu no matemātiskajām operācijām (izmantojot switch).
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int x, y;
+    char z;
+    
+    cout<<" ievadiet pirmo skaitli: "<< endl;
+    cin>> x;
+    
+    cout << " ievadiet otro skaitli: "<< endl;
+    cin>> y;
+    
+    cout<< " ievadiet darbības veidu +, -, *"<<endl;
+    
+    cin>>z;
+    
+    switch (z) {
+        case '+':
+            cout<<x<<"+"<<y<<"="<<x+y;
+            break;
+            
+        case '-':
+            cout<<x<<"-"<<y<<"="<<x-y;
+            break;
+            
+        case '*':
+            cout<<x<<"*"<<y<<"="<<x*y;
+            break;
+            
+        default:
+            break;
+    }
+    
+    return 0;
+}
